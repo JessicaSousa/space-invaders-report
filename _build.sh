f3949c89a3d6cc79762bfb0c01def33985ac69d4
Jessica Sousa <jessicacardosops@gmail.com>
2020-02-11 20:31:23 -0300

    atualizar arquivos

--- _build.sh
+++ _build.sh
@@ -2,6 +2,7 @@
 
 set -ev
 
+sudo apt install texlive-lang-portuguese
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"

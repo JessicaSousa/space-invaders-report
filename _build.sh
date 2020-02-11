f5dd2cecfd84b7033552ad6087d709478ab5dc51
Jessica Sousa <jessicacardosops@gmail.com>
2020-02-11 20:40:53 -0300

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -2,7 +2,8 @@
 
 set -ev
 
-sudo apt install texlive-lang-portuguese
+Rscript -e "install.packages('tinytex')"
+Rscript -e "tinytex::install_tinytex()"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"

0328eaa690e51984eb268307c3d871c546657fe6
Jessica Sousa <jessicacardosops@gmail.com>
2020-02-11 22:41:10 -0300

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -4,6 +4,7 @@ set -ev
 
 Rscript -e "install.packages('tinytex')"
 Rscript -e "tinytex::install_tinytex()"
+Rscript -e "tinytex::tlmgr_install('babel-portuges')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
